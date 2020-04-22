0d163037b1008ef8fec6b0786881b23ef35f7dda
root <root@DESKTOP-1F75T9I.localdomain>
2020-04-22 18:48:40 -0400

    Allow user to send text

---
+++ src/TextSocket.java
@@ -0,0 +1,37 @@
+import hypercast.*;
+
+import java.io.IOException;
+
+public class TextSocket implements I_ReceiveCallback {
+
+    private I_OverlaySocket socket;
+
+    @Override
+    public void ReceiveCallback(I_OverlayMessage i_overlayMessage) {
+        byte[] data = i_overlayMessage.getPayload();
+        System.out.println(new String(data));
+    }
+
+    public TextSocket(String configFile) {
+        HyperCastConfig config = HyperCastConfig.createConfig(configFile);
+        socket = config.createOverlaySocket(this);
+        socket.joinOverlay();
+    }
+
+    public void sendToAll(String text) {
+        byte[] data = text.getBytes();
+        I_OverlayMessage message = socket.createMessage(data);
+        socket.sendToAll(message);
+    }
+
+    public void sendTo(String text, I_LogicalAddress logicalAddress) {
+        byte[] data = text.getBytes();
+        I_OverlayMessage message = socket.createMessage(data);
+        socket.sendToNode(message, logicalAddress);
+    }
+
+    public static void main(String[] args) {
+        TextSocket textSocket = new TextSocket("C:\\Users\\wusix\\Documents\\courses\\csc299\\CampusTour\\config\\hypercast.xml");
+        textSocket.sendToAll("Hello World");
+    }
+}
